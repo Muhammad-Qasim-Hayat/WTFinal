1812c4b20fa77fe89aa9c5c7c9a9cbff3909ed01
Muhammad-Qasim-Hayat <63369083+Muhammad-Qasim-Hayat@users.noreply.github.com>
2021-06-22 23:55:47 +0500

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function (err, req, res, next) {
 });
 
 mongoose
-  .connect("mongodb+srv://admin:admin@cluster0.qz3p3.mongodb.net/test", {
+  .connect("mongodb+srv://admin:admin@cluster0.qz3p3.mongodb.net/Terminal", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
