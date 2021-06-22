9bf0967379320ebc0b7e981543ed110a5c27e3fb
Muhammad-Qasim-Hayat <63369083+Muhammad-Qasim-Hayat@users.noreply.github.com>
2021-06-22 11:40:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function (err, req, res, next) {
 });
 
 mongoose
-  .connect("APNI INFO DALO", {
+  .connect("mongodb+srv://admin:admin@cluster0.qz3p3.mongodb.net/Terminal", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
